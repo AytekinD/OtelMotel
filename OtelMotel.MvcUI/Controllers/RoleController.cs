aa41be058cc56c6ca30423b8b5979af5edabd80c
AytekinD <aytekindondurmaci@hotmail.com>
2022-12-14 20:14:56 +0300

    role Controller olusturuldu

--- OtelMotel.MvcUI/Controllers/RoleController.cs
+++ OtelMotel.MvcUI/Controllers/RoleController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OtelMotel.BL.Abstract;
 
 namespace OtelMotel.MvcUI.Controllers
 {
     public class RoleController : Controller
     {
-        public IActionResult Index()
+        private readonly IRoleManager roleMAnager;
+
+        public RoleController(IRoleManager roleMAnager)
         {
-            return View();
+            this.roleMAnager = roleMAnager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await roleMAnager.FindAllAsync();
+            return View(result);
         }
     }
 }
