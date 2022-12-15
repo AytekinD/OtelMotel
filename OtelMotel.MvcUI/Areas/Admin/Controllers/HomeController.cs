e5a41fe816b579b7ced1b2e50ac8340b07111ede
AytekinD <aytekindondurmaci@hotmail.com>
2022-12-15 19:22:48 +0300

    TEmplete giydirme devam

---
+++ OtelMotel.MvcUI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OtelMotel.MvcUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
