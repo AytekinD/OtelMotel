19558e9166737027f15070f12007cd20001a6498
AytekinD <aytekindondurmaci@hotmail.com>
2023-01-27 16:31:13 +0300

    OdaControllerda updateolusturuldu.

--- OtelMotel.MvcUI/Areas/Admin/Controllers/OdaController.cs
+++ OtelMotel.MvcUI/Areas/Admin/Controllers/OdaController.cs
@@ -5,55 +5,90 @@ using OtelMotel.MvcUI.Areas.Admin.Models.Oda;
 
 namespace OtelMotel.MvcUI.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+	[Area("Admin")]
 
-    public class OdaController : Controller
-    {
-        private readonly IOdaManager odaManager;
+	public class OdaController : Controller
+	{
+		private readonly IOdaManager odaManager;
 
-        public OdaController(IOdaManager odaManager)
-        {
-            this.odaManager = odaManager;
-        }
-        public async Task<IActionResult> Index()
-        {
-            var result = await odaManager.FindAllAsync(null);
-            return View(result);
-        }
-        [HttpGet]
-        public async Task<IActionResult> Create()
-        {
+		public OdaController(IOdaManager odaManager)
+		{
+			this.odaManager = odaManager;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var result = await odaManager.FindAllAsync(null);
+			return View(result);
+		}
+		[HttpGet]
+		public async Task<IActionResult> Create()
+		{
 
-            var createDto = new OdaCreateDTO();
+			var createDto = new OdaCreateDTO();
 
-            return View(createDto);
-        }
-        [HttpPost]
-        public async Task<IActionResult> Create(OdaCreateDTO createDTO)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(createDTO);
-            }
+			return View(createDto);
+		}
+		[HttpPost]
+		public async Task<IActionResult> Create(OdaCreateDTO createDTO)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(createDTO);
+			}
 
-            var oda = new Oda()
-            {
-                OdaNo = createDTO.OdaNo,
-                KisiSayisi = createDTO.KisiSayisi,
-                Durum = createDTO.Durum,
+			var oda = new Oda()
+			{
+				OdaNo = createDTO.OdaNo,
+				KisiSayisi = createDTO.KisiSayisi,
+				Durum = createDTO.Durum,
 
-            };
+			};
 
 
-            var sonuc = await odaManager.CreateAsync(oda);
-            if (sonuc > 0)
-            {
-                return RedirectToAction("Index", "Musteri");
-            }
-            else
-            {
-                ModelState.AddModelError("", "Bilinmeyen bir hata olustu.Deaha sonra tekra deneyibiz"); return View(oda);
-            }
-        }
-    }
+			var sonuc = await odaManager.CreateAsync(oda);
+			if (sonuc > 0)
+			{
+				return RedirectToAction("Index", "Musteri");
+			}
+			else
+			{
+				ModelState.AddModelError("", "Bilinmeyen bir hata olustu.Deaha sonra tekra deneyibiz"); return View(oda);
+			}
+		}
+		[HttpGet]
+		public async Task<IActionResult> Update(Guid id)
+		{
+			var oda = await odaManager.FindAsync(p => p.Id == id);
+			OdaUpdateDTO updateDto = new OdaUpdateDTO
+			{
+				OdaNo = oda.OdaNo,
+				Durum = oda.Durum,
+				KisiSayisi = oda.KisiSayisi
+			};
+			return View(updateDto);
+		}
+		[HttpPost]
+		public async Task<IActionResult> Update(OdaUpdateDTO odaUpdateDTO)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(odaUpdateDTO);
+			}
+			var oda = await odaManager.FindAsync(p => p.Id == odaUpdateDTO.Id);
+			oda.OdaNo = odaUpdateDTO.OdaNo;
+			oda.Durum = odaUpdateDTO.Durum;
+			oda.KisiSayisi = odaUpdateDTO.KisiSayisi;
+
+			var sonuc = await odaManager.UpdateAsync(oda);
+			if (sonuc > 0)
+			{
+				return RedirectToAction("Index", "Oda");
+			}
+			else
+			{
+				ModelState.AddModelError("", "Bilinmeyen bir hata olustu.Tekrar deneyiniz.");
+				return View(odaUpdateDTO);
+			}
+		}
+	}
 }
--- OtelMotel.MvcUI/Areas/Admin/Models/Oda/OdaUpdateDTO.cs
+++ OtelMotel.MvcUI/Areas/Admin/Models/Oda/OdaUpdateDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace OtelMotel.MvcUI.Areas.Admin.Models.Oda
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OtelMotel.MvcUI.Areas.Admin.Models.Oda
 {
-    public class OdaUpdateDTO
-    {
-    }
+	public class OdaUpdateDTO
+	{
+		public Guid Id { get; set; }
+
+		[Required(AllowEmptyStrings = false, ErrorMessage = "Oda Zorunlu Alandir")]
+		[DataType(DataType.Text)]
+		public string OdaNo { get; set; }
+		public byte KisiSayisi { get; set; }
+		public bool Durum { get; set; } = true;
+	}
 }
