fadb5f0b99914dfde0fe579ad4ce62986321b9b4
AytekinD <aytekindondurmaci@hotmail.com>
2023-01-30 18:39:07 +0300

    Musteri SoftDelete Çalıştırıldı

--- OtelMotel.MvcUI/Areas/Admin/Controllers/MusteriController.cs
+++ OtelMotel.MvcUI/Areas/Admin/Controllers/MusteriController.cs
@@ -1,134 +1,131 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OtelMotel.BL.Abstract;
+using OtelMotel.Entities.Entities.Abstract;
 using OtelMotel.Entities.Entities.Concrete;
 using OtelMotel.MvcUI.Areas.Admin.Models.Musteri;
 using System.Security.Claims;
 
 namespace OtelMotel.MvcUI.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    [Authorize(Roles = "Admin")]
+	[Area("Admin")]
+	[Authorize(Roles = "Admin")]
 
-    public class MusteriController : Controller
-    {
-        private readonly IMusteriManager musteriManager;
+	public class MusteriController : Controller
+	{
+		private readonly IMusteriManager musteriManager;
 
-        public MusteriController(IMusteriManager musteriManager)
-        {
-            this.musteriManager = musteriManager;
-        }
-        public async Task<IActionResult> Index()
-        {
-            var result = await musteriManager.FindAllAsync(null);
-            return View(result.ToList());
-        }
-        [HttpGet]
-        public async Task<IActionResult> Create()
-        {
+		public MusteriController(IMusteriManager musteriManager)
+		{
+			this.musteriManager = musteriManager;
+		}
+		public async Task<IActionResult> Index()
+		{
 
-            var createDto = new MusteriCreateDTO();
-
-            return View(createDto);
-        }
+			var result = await musteriManager.FindAllAsync(p => p.Status == Status.Active || p.Status == Status.Update);
+			return View(result.ToList());
 
 
-        [HttpPost]
-        public async Task<IActionResult> Create(MusteriCreateDTO createDTO)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(createDTO);
-            }
+		}
+		[HttpGet]
+		public async Task<IActionResult> Create()
+		{
 
-            var musteri = new Musteri()
-            {
-                Ad = createDTO.Ad,
-                Soyad = createDTO.Soyad,
-                MusteriTcNo = createDTO.MusteriTcNo,
-                CepNo = createDTO.CepNo,
-                Cinsiyet = createDTO.Cinsiyet,
-                KullaniciId = Guid.Parse(User.Claims.FirstOrDefault(p => p.Type == ClaimTypes.NameIdentifier).Value)
-            };
+			var createDto = new MusteriCreateDTO();
+
+			return View(createDto);
+		}
 
 
-            var sonuc = await musteriManager.CreateAsync(musteri);
-            if (sonuc > 0)
-            {
-                return RedirectToAction("Index", "Musteri");
-            }
-            else
-            {
-                ModelState.AddModelError("", "Bilinmeyen bir hata olustu.Deaha sonra tekra deneyibiz"); return View(musteri);
-            }
-        }
+		[HttpPost]
+		public async Task<IActionResult> Create(MusteriCreateDTO createDTO)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(createDTO);
+			}
 
-        [HttpGet]
-        public async Task<IActionResult> Update(Guid Id)
-        {
-            var musteri = await musteriManager.FindAsync(p => p.Id == Id);
-            MusteriUpdateDTO updateDto = new MusteriUpdateDTO
-            {
-                Id = musteri.Id,
-                Ad = musteri.Ad,
-                Soyad = musteri.Soyad,
-                MusteriTcNo = musteri.MusteriTcNo,
-                CepNo = musteri.CepNo,
-                Cinsiyet = musteri.Cinsiyet,
-            };
-            return View(updateDto);
-        }
+			var musteri = new Musteri()
+			{
+				Ad = createDTO.Ad,
+				Soyad = createDTO.Soyad,
+				MusteriTcNo = createDTO.MusteriTcNo,
+				CepNo = createDTO.CepNo,
+				Cinsiyet = createDTO.Cinsiyet,
+				KullaniciId = Guid.Parse(User.Claims.FirstOrDefault(p => p.Type == ClaimTypes.NameIdentifier).Value)
+			};
 
 
-        [HttpPost]
-        public async Task<IActionResult> Update(MusteriUpdateDTO updateDTO)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(updateDTO);
-            }
-            var musteri = musteriManager.FindAsync(p => p.Id == updateDTO.Id).Result;
+			var sonuc = await musteriManager.CreateAsync(musteri);
+			if (sonuc > 0)
+			{
+				return RedirectToAction("Index", "Musteri");
+			}
+			else
+			{
+				ModelState.AddModelError("", "Bilinmeyen bir hata olustu.Deaha sonra tekra deneyibiz"); return View(musteri);
+			}
+		}
 
-            musteri.Ad = updateDTO.Ad;
-            musteri.Soyad = updateDTO.Soyad;
-            musteri.MusteriTcNo = updateDTO.MusteriTcNo;
-            musteri.CepNo = updateDTO.CepNo;
-            musteri.Cinsiyet = updateDTO.Cinsiyet;
-            //musteri.KullaniciId = updateDTO.KullaniciId;
+		[HttpGet]
+		public async Task<IActionResult> Update(Guid Id)
+		{
+			var musteri = await musteriManager.FindAsync(p => p.Id == Id);
+			MusteriUpdateDTO updateDto = new MusteriUpdateDTO
+			{
+				Id = musteri.Id,
+				Ad = musteri.Ad,
+				Soyad = musteri.Soyad,
+				MusteriTcNo = musteri.MusteriTcNo,
+				CepNo = musteri.CepNo,
+				Cinsiyet = musteri.Cinsiyet,
+			};
+			return View(updateDto);
+		}
 
-            var sonuc = await musteriManager.UpdateAsync(musteri);
-            if (sonuc > 0)
-            {
-                return RedirectToAction("Index", "Musteri");
-            }
-            else
-            {
-                ModelState.AddModelError("", "Bilinmeyen bir hata olustu.Tekrar deneyiniz.");
-                return View(updateDTO);
-            }
-        }
 
-        [HttpGet]
-        public async Task<IActionResult> Delete(Guid Id)
-        {
-            var musteri = await musteriManager.FindAsync(p => p.Id == Id);
-            return RedirectToAction("Index", "Musteri");
+		[HttpPost]
+		public async Task<IActionResult> Update(MusteriUpdateDTO updateDTO)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(updateDTO);
+			}
+			var musteri = musteriManager.FindAsync(p => p.Id == updateDTO.Id).Result;
 
-        }
-        [HttpPost]
-        public async Task<IActionResult> Delete(Musteri musteri)
-        {
-            musteri = await musteriManager.FindAsync(p => p.Id == musteri.Id);
-            var sonuc = await musteriManager.DeleteAsync(musteri);
-            if (sonuc > 0)
-            {
-                return RedirectToAction("Index", "Musteri");
-            }
-            else
-            {
-                ModelState.AddModelError("", "Bilinmeyen bir hata olustu.Tekrar deneyiniz.");
-                return View(musteri);
-            }
-        }
-    }
+			musteri.Ad = updateDTO.Ad;
+			musteri.Soyad = updateDTO.Soyad;
+			musteri.MusteriTcNo = updateDTO.MusteriTcNo;
+			musteri.CepNo = updateDTO.CepNo;
+			musteri.Cinsiyet = updateDTO.Cinsiyet;
+			//musteri.KullaniciId = updateDTO.KullaniciId;
+
+			var sonuc = await musteriManager.UpdateAsync(musteri);
+			if (sonuc > 0)
+			{
+				return RedirectToAction("Index", "Musteri");
+			}
+			else
+			{
+				ModelState.AddModelError("", "Bilinmeyen bir hata olustu.Tekrar deneyiniz.");
+				return View(updateDTO);
+			}
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Delete(Guid Id)
+		{
+			var musteri = await musteriManager.FindAsync(p => p.Id == Id);
+			return View(musteri);
+
+		}
+		[HttpPost]
+		public async Task<IActionResult> Delete(Musteri musteri)
+		{
+			var musteridelete = await musteriManager.FindAsync(p => p.Id == musteri.Id);
+			musteriManager.DeleteAsync(musteridelete);
+
+			return RedirectToAction("Index", "Musteri");
+		}
+	}
 }
